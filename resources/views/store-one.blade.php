1a79b6afddd02607e3355275344c2a432ffc9da2
Nazrin Putra <putra_naz94@yahoo.com>
2021-04-25 21:07:30 +0800

    Open in same tab, add back button

--- resources/views/store-one.blade.php
+++ resources/views/store-one.blade.php
@@ -1,1 +1,2 @@
 <h1>This is store 1 link</h1>
+<button type="button"><a href="/">Back</a></button>
--- resources/views/store-two.blade.php
+++ resources/views/store-two.blade.php
@@ -1,1 +1,2 @@
 <h1>This is store 2 link</h1>
+<button type="button"><a href="/">Back</a></button>
