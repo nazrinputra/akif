e4b31fafa1f254986ef5abeb2a7aafd6ea446e11
Nazrin Putra <putra_naz94@yahoo.com>
2021-05-08 21:38:19 +0800

    Navbar use logo with alt text instead of text

--- resources/views/components/navbar-store.blade.php
+++ resources/views/components/navbar-store.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="/">Akif</a>
+        <a class="navbar-brand js-scroll-trigger" href="/"><img class="img-thumbnail" src="img/logo-navbar.png" alt="Akif" /></a>
         <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
