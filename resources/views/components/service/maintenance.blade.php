6ab185116ec221089c74582578185c2991def0fb
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-08 17:24:02 +0800

    Extract modals to separate components

---
+++ resources/views/components/service/maintenance.blade.php
@@ -0,0 +1,32 @@
+<div class="service-modal modal fade" id="serviceModal2" tabindex="-1" role="dialog" aria-labelledby="serviceModal2Label" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-body text-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <!-- Service Modal - Title-->
+                            <h2 class="service-modal-title text-secondary text-uppercase mb-0" id="serviceModal2Label">Maintenance</h2>
+                            <!-- Icon Divider-->
+                            <x-divider></x-divider>
+                            <p class="mb-5 text-uppercase">
+                                Conditioner<br>
+                                Glaze & wax<br>
+                                Paint coatings<br>
+                                Protection wax<br>
+                                Engine bay wash<br>
+                                Sanitize nano mist<br>
+                                Paint protective films<br>
+                                Inner plastic & leather
+                            </p>
+                            <button class="btn btn-primary" data-dismiss="modal">
+                                <i class="fas fa-times fa-fw"></i>
+                                Close Window
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
