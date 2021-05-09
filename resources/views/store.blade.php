8e63ee553b1524d3613f93731d3d35aaf6740515
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-09 21:57:55 +0800

    Filter queues and pass to queue-table

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -1,3 +1,9 @@
+@php
+    $queuing = $store->queues->where('status', 'Queuing');
+    $grooming = $store->queues->where('status', 'Grooming');
+    $completed = $store->queues->where('status', 'Completed');
+@endphp
+
 <x-layout title="Akif - {{ $store->name }}" :stores="$stores">
     <!-- Queue Section-->
     <section class="page-section" id="{{ $store->slug }}">
@@ -9,20 +15,11 @@
             <!-- Queue Subheading-->
             <div class="row text-uppercase">
                 <!-- Queue Table 1 -->
-                <x-queue-table class="table-danger">
-                    <x-slot name="status">Queuing</x-slot>
-                    <h4>ABC1234</h4>
-                </x-queue-table>
+                <x-queue-table class="table-danger" status="Queuing" :queues="$queuing" />
                 <!-- Queue Table 2 -->
-                <x-queue-table class="table-warning">
-                    <x-slot name="status">Grooming</x-slot>
-                    <h4>ABC1234</h4>
-                </x-queue-table>
+                <x-queue-table class="table-warning" status="Grooming" :queues="$grooming" />
                 <!-- Queue Table 3 -->
-                <x-queue-table class="table-success">
-                    <x-slot name="status">Completed</x-slot>
-                    <h4>ABC1234</h4>
-                </x-queue-table>
+                <x-queue-table class="table-success" status="Completed" :queues="$completed" />
             </div>
         </div>
     </section>
