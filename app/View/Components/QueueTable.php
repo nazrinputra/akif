f640189efe8679515a506eefb6256c2b2d357966
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-09 21:57:26 +0800

    Declare properties for component

--- app/View/Components/QueueTable.php
+++ app/View/Components/QueueTable.php
@@ -6,14 +6,19 @@ use Illuminate\View\Component;
 
 class QueueTable extends Component
 {
+    public $status;
+
+    public $queues;
+
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($status, $queues)
     {
-        //
+        $this->status = $status;
+        $this->queues = $queues;
     }
 
     /**
