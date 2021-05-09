3fbcce17fe50abe05d25afc6bf3e259c40ab00cd
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-09 15:28:52 +0800

    Use store model

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -1,39 +1,27 @@
 <x-layout>
-    <x-slot name="title">Akif - {{ ucwords($name) }}</x-slot>
+    <x-slot name="title">Akif - {{ $store->name }}</x-slot>
     <!-- Queue Section-->
-    <section class="page-section" id="{{ $slug }}">
+    <section class="page-section" id="{{ $store->slug }}">
         <div class="container">
             <!-- Queue Section Heading-->
-            <h2 class="page-section-heading text-secondary text-center text-uppercase pt-5">{{ $name }}</h2>
+            <h2 class="page-section-heading text-secondary text-center text-uppercase pt-5">{{ $store->name }}</h2>
             <!-- Icon Divider-->
             <x-divider/>
             <!-- Queue Subheading-->
             <div class="row text-uppercase">
                 <!-- Queue Table 1 -->
                 <x-queue-table class="table-danger">
-                    <x-slot name="status">
-                        Queuing <i class="fas fa-hourglass-start"></i>
-                    </x-slot>
-                    <h4>ABC1234</h4>
-                    <h4>ABC1234</h4>
-                    <h4>ABC1234</h4>
+                    <x-slot name="status">Queuing</x-slot>
                     <h4>ABC1234</h4>
                 </x-queue-table>
                 <!-- Queue Table 2 -->
                 <x-queue-table class="table-warning">
-                    <x-slot name="status">
-                        Queuing <i class="fas fa-hands-wash"></i>
-                    </x-slot>
-                    <h4>ABC1234</h4>
+                    <x-slot name="status">Grooming</x-slot>
                     <h4>ABC1234</h4>
                 </x-queue-table>
                 <!-- Queue Table 3 -->
                 <x-queue-table class="table-success">
-                    <x-slot name="status">
-                        Queuing <i class="fas fa-clipboard-check"></i>
-                    </x-slot>
-                    <h4>ABC1234</h4>
-                    <h4>ABC1234</h4>
+                    <x-slot name="status">Completed</x-slot>
                     <h4>ABC1234</h4>
                 </x-queue-table>
             </div>
