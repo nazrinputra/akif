1139da59520b174cca6ec2bf4d6f51198ae69369
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-09 20:12:56 +0800

    Pass stores for navbar looping

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 
 Route::get('store/{store:slug}', function (Store $store) {
     return view('store', [
+        'stores' => Store::all(),
         'store' => $store
     ]);
 });
