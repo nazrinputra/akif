a98b54b63da58b125f29b3442790dbffa6d0c27b
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-09 09:17:54 +0800

    Use new queue component style

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-navbar-store></x-navbar-store>
+    <x-navbar/>
     <!-- Queue Section-->
     <section class="page-section" id="{{ $slug }}">
         <div class="container">
@@ -10,11 +10,32 @@
             <!-- Queue Subheading-->
             <div class="row text-uppercase">
                 <!-- Queue Table 1 -->
-                <x-queue-table status="Queuing <i class='fas fa-hourglass-start'></i>" table="table-danger"/>
+                <x-queue-table class="table-danger">
+                    <x-slot name="status">
+                        Queuing <i class="fas fa-hourglass-start"></i>
+                    </x-slot>
+                    <h4>ABC1234</h4>
+                    <h4>ABC1234</h4>
+                    <h4>ABC1234</h4>
+                    <h4>ABC1234</h4>
+                </x-queue-table>
                 <!-- Queue Table 2 -->
-                <x-queue-table status="Grooming <i class='fas fa-hands-wash'></i>" table="table-warning"/>
+                <x-queue-table class="table-warning">
+                    <x-slot name="status">
+                        Queuing <i class="fas fa-hands-wash"></i>
+                    </x-slot>
+                    <h4>ABC1234</h4>
+                    <h4>ABC1234</h4>
+                </x-queue-table>
                 <!-- Queue Table 3 -->
-                <x-queue-table status="Completed <i class='fas fa-clipboard-check'></i>" table="table-success"/>
+                <x-queue-table class="table-success">
+                    <x-slot name="status">
+                        Queuing <i class="fas fa-clipboard-check"></i>
+                    </x-slot>
+                    <h4>ABC1234</h4>
+                    <h4>ABC1234</h4>
+                    <h4>ABC1234</h4>
+                </x-queue-table>
             </div>
         </div>
     </section>
