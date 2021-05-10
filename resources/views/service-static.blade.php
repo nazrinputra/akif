699076be2124a5b2d0a5c6cdc5791c3a9d4e7ad8
nisa-sabrina <nisa.sabrina94@yahoo.com>
2021-05-11 00:19:49 +0800

    Edit service static page

--- resources/views/service-static.blade.php
+++ resources/views/service-static.blade.php
@@ -1,27 +1,95 @@
 <x-layout title="Akif - Service Name" :stores="$stores">
     <!-- Service Section-->
-    <section class="page-section bg-primary" id="service">
-        <div class="container text-center text-uppercase">
+    <section class="page-section" id="service">
+        <div class="container text-uppercase">
+
             <!-- Service Section Heading-->
-            <h2 class="page-section-heading text-secondary pt-5">Service Name</h2>
-            <!-- Icon Divider-->
-            <x-divider/>
-            <!-- Service Price-->
             <div class="row">
-                <h3 class="col col-lg-3 col-md-6">RM10</h3>
-                <h3 class="col col-lg-3 col-md-6">RM20</h3>
-                <h3 class="col col-lg-3 col-md-6">RM30</h3>
-                <h3 class="col col-lg-3 col-md-6">RM40</h3>
+                <div class="col">
+                    <h2 class="text-center text-secondary pt-5">Seat & Interiors Cleaning</h2>
+                </div>
             </div>
+
+            <x-divider/>
+
+            <div class="row text-center">
+                <div class="col">
+                    <h5>Duration: 10 min</h5>
+                </div>
+                <div class="col">
+                    <h5>Frequency: When neccesary</h5>
+                </div>
+            </div>
+
+            <!-- Service Detail Excerpt-->
+            <div class="row mt-5">
+                <div class="col">
+                    <h3>Lorem ipsum</h3>
+                </div>
+            </div>
+            
             <!-- Service Subheading-->
             <div class="row">
-                <p class="masthead-subheading font-weight-light pt-5 mb-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                </p>
+                <div class="col">
+                    <p class="text-left font-weight-light">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        <a href="/service-detail" class="text-secondary">Learn more..</a>
+                    </p>
+                </div>
+            </div>
+
+            <!-- Service Package -->
+            <div class="row">
+                <div class="col">
+                    <h3>Package</h3>
+                    <ol>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                    </ol>
+                </div>
+            </div>
+
+            
+            <!-- Service Price -->
+            <div class="row">
+                <div class="col">
+                    <h3>Price</h3>
+
+                    <table>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </table>
+                    <table class="table table-bordered">
+                        <tr>
+                            <th rowspan="3">Kesemua kerusi fabrik/kulit</th>
+                            <td>Small = RM10</td>
+                        </tr>
+                        <tr>
+                            <td>Medium = RM20</td>
+                        </tr>
+                        <tr>
+                            <td>Large = RM30</td>
+                        </tr>
+                        <tr>
+                            <th>Boot</th>
+                            <td>RM 20</td>
+                        </tr>
+                        <tr>
+                            <th>Panel Pintu</th>
+                            <td>RM 50</td>
+                        </tr>
+                        <tr>
+                            <th>Karpet</th>
+                            <td>RM 6</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </section>
