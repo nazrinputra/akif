1b7e1c1f59482c539900d6a71fbf3f16be64bf8b
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-12 23:11:05 +0800

    Delete previous models, views, migrations and seeders

--- resources/views/components/divider.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="divider-custom">
-    <div class="divider-custom-line"></div>
-    <div class="divider-custom-icon">
-        <i class="fas fa-car"></i>
-    </div>
-    <div class="divider-custom-line"></div>
-</div>
