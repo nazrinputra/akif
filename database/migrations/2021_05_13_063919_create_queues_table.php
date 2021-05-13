bf92fa3eafa1588beccbe8abd5685ce99657d652
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-13 21:31:51 +0800

    Add foreign id index to queue table

--- database/migrations/2021_05_13_063919_create_queues_table.php
+++ database/migrations/2021_05_13_063919_create_queues_table.php
@@ -15,8 +15,8 @@ class CreateQueuesTable extends Migration
     {
         Schema::create('queues', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('store_id')->nullable();
-            $table->foreignId('car_id')->nullable();
+            $table->foreignId('store_id')->nullable()->constrained();
+            $table->foreignId('car_id')->nullable()->constrained();
             $table->foreignId('customer_id')->nullable();
             $table->foreignId('package_id')->nullable();
             $table->string('status');
