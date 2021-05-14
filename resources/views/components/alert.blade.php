e646bafe7d0282eb2d26d73e5f7a82f8511ddd87
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-14 23:00:32 +0800

    Align alert center

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <div {{ $attributes([ 'class' => 'alert alert-dismissible fade show']) }} role="alert">
+    <div {{ $attributes([ 'class' => 'alert alert-dismissible fade show text-center']) }} role="alert">
         {!! $slot !!}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
