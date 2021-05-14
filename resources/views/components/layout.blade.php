549fb51e962b50229042936edd9d6a535c2f7cfd
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-14 21:02:32 +0800

    Use new component x-* view

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Title-->
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico" />
+
+    <!-- Mix CSS-->
+    <link rel="stylesheet" href="{{mix ('css/app.css')}}">
+</head>
+
+<body>
+    <div id="app">
+        <x-navbar />
+
+        <main class="pt-5">
+            {{ $slot }}
+        </main>
+    </div>
+
+    <!-- Mix JS-->
+    <script src="{{mix ('js/app.js')}}"></script>
+
+</body>
+
+<!-- Footer section-->
+<footer class="footer text-center">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Social Icons-->
+            <div class="col-lg mb-lg-0">
+                <h4 class="text-uppercase">Contact Us</h4>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/akifcarwash/">
+                    <i class="fab fa-fw fa-facebook-f"></i>
+                </a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://api.whatsapp.com/send?phone=60136541240">
+                    <i class="fab fa-fw fa-whatsapp"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Copyright Section-->
+<div class="copyright py-4 text-center text-white">
+    <div class="container"><small>Copyright ©
+            @guest
+            <a href="{{ url('/login') }}" class="text-primary">Akif Car Grooming Center</a>
+            @else
+            <a href="{{ url('/logout') }}" class="text-primary">Akif Car Grooming Center</a>
+            @endguest
+            2021</small></div>
+</div>
+
+{{-- FIXME remove after finish development --}}
+<x-alert class="alert-info fixed-bottom mb-0">
+    <strong>Hey there!</strong> This site is still under development. Any data you see here is not real.
+</x-alert>
+
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,81 +1,83 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container mt-5 py-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    <div class="row bg-success py-3">
-                        <div class="col d-flex justify-content-center">
-                            <a class="btn btn-outline-dark btn-social mx-1" href="#">
-                                <i class="fa fa-cash-register"></i>
-                            </a>
-                            <p class="ml-3 mt-1 pt-2">Counter Menu</p>
-                        </div>
-                        <div class="col d-flex justify-content-center">
-                            <a class="btn btn-outline-dark btn-social mx-1" href="#">
-                                <i class="fa fa-calculator"></i>
-                            </a>
-                            <p class="ml-3 mt-1 pt-2">Modify Whatsapp Text</p>
-                        </div>
+<x-layout>
+    <div class="container mt-5 py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <span class="my-auto">Dashboard</span>
+                        <button type="button" class="btn btn-primary"><a href="{{ url('/logout') }}" class="text-white"
+                                style="text-decoration: none">Logout</a></button>
                     </div>
 
-                    <div class="row bg-success py-3">
-                        <div class="col d-flex justify-content-center">
-                            <a class="btn btn-outline-dark btn-social mx-1" href="#">
-                                <i class="fa fa-cash-register"></i>
-                            </a>
-                            <p class="ml-3 mt-1 pt-2">Manage Cars</p>
+                    <div class="card-body">
+                        @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
                         </div>
-                        <div class="col d-flex justify-content-center">
-                            <a class="btn btn-outline-dark btn-social mx-1" href="#">
-                                <i class="fa fa-calculator"></i>
-                            </a>
-                            <p class="ml-3 mt-1 pt-2">Manage Customers</p>
-                        </div>
-                    </div>
+                        @endif
 
-                    <div class="row mt-3 bg-warning py-3">
-                        <div class="col d-flex justify-content-center">
-                            <a class="btn btn-outline-dark btn-social mx-1" href="#">
-                                <i class="fa fa-key"></i>
-                            </a>
-                            <p class="ml-3 mt-1 pt-2">Manage Services</p>
+                        <div class="row bg-success py-3">
+                            <div class="col d-flex justify-content-center">
+                                <a class="btn btn-outline-dark btn-social mx-1" href="#">
+                                    <i class="fa fa-cash-register"></i>
+                                </a>
+                                <p class="ml-3 mt-1 pt-2">Counter Menu</p>
+                            </div>
+                            <div class="col d-flex justify-content-center">
+                                <a class="btn btn-outline-dark btn-social mx-1" href="#">
+                                    <i class="fa fa-calculator"></i>
+                                </a>
+                                <p class="ml-3 mt-1 pt-2">Modify Whatsapp Text</p>
+                            </div>
                         </div>
-                        <div class="col d-flex justify-content-center">
-                            <a class="btn btn-outline-dark btn-social mx-1" href="#">
-                                <i class="fa fa-desktop"></i>
-                            </a>
-                            <p class="ml-3 mt-1 pt-2">Manage Packages</p>
-                        </div>
-                    </div>
 
-                    <div class="row mt-3 bg-danger py-3">
-                        <div class="col d-flex justify-content-center">
-                            <a class="btn btn-outline-dark btn-social mx-1" href="#">
-                                <i class="fa fa-folder-open"></i>
-                            </a>
-                            <p class="ml-3 mt-1 pt-2">Dashboard Reports</p>
+                        <div class="row bg-success py-3">
+                            <div class="col d-flex justify-content-center">
+                                <a class="btn btn-outline-dark btn-social mx-1" href="#">
+                                    <i class="fa fa-cash-register"></i>
+                                </a>
+                                <p class="ml-3 mt-1 pt-2">Manage Cars</p>
+                            </div>
+                            <div class="col d-flex justify-content-center">
+                                <a class="btn btn-outline-dark btn-social mx-1" href="#">
+                                    <i class="fa fa-calculator"></i>
+                                </a>
+                                <p class="ml-3 mt-1 pt-2">Manage Customers</p>
+                            </div>
                         </div>
-                        <div class="col d-flex justify-content-center">
-                            <a class="btn btn-outline-dark btn-social mx-1" href="#">
-                                <i class="fa fa-hourglass"></i>
-                            </a>
-                            <p class="ml-3 mt-1 pt-2">Manage Staffs</p>
+
+                        <div class="row mt-3 bg-warning py-3">
+                            <div class="col d-flex justify-content-center">
+                                <a class="btn btn-outline-dark btn-social mx-1" href="#">
+                                    <i class="fa fa-key"></i>
+                                </a>
+                                <p class="ml-3 mt-1 pt-2">Manage Services</p>
+                            </div>
+                            <div class="col d-flex justify-content-center">
+                                <a class="btn btn-outline-dark btn-social mx-1" href="#">
+                                    <i class="fa fa-desktop"></i>
+                                </a>
+                                <p class="ml-3 mt-1 pt-2">Manage Packages</p>
+                            </div>
+                        </div>
+
+                        <div class="row mt-3 bg-danger py-3">
+                            <div class="col d-flex justify-content-center">
+                                <a class="btn btn-outline-dark btn-social mx-1" href="#">
+                                    <i class="fa fa-folder-open"></i>
+                                </a>
+                                <p class="ml-3 mt-1 pt-2">Dashboard Reports</p>
+                            </div>
+                            <div class="col d-flex justify-content-center">
+                                <a class="btn btn-outline-dark btn-social mx-1" href="#">
+                                    <i class="fa fa-hourglass"></i>
+                                </a>
+                                <p class="ml-3 mt-1 pt-2">Manage Staffs</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</x-layout>
