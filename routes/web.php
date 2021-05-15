9ec568f9bc912cb577240fba1211af419db4e8d7
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-15 19:30:28 +0800

    Clear cache on sensitive data

--- routes/web.php
+++ routes/web.php
@@ -23,56 +23,59 @@ Route::get('/', function () {
 
 Route::get('store/{store:slug}', 'StoreController@show')->name('store');
 
+Route::get('promotions', function () {
+    return view('public.promotions');
+})->name('promotions');
+
 Auth::routes();
 
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/index', 'HomeController@index')->name('index');
 
-Route::get('counter', function () {
-    return view('private.counter');
-})->name('counter');
+Route::group(['middleware' => ['auth', 'clear.cache']], function () {
 
-Route::get('whatsapp', function () {
-    return view('private.whatsapp');
-})->name('whatsapp');
+    Route::get('counter', function () {
+        return view('private.counter');
+    })->name('counter');
 
-Route::get('cars', 'CarController@index')->name('cars');
+    Route::get('whatsapp', function () {
+        return view('private.whatsapp');
+    })->name('whatsapp');
 
-Route::get('cars/{car:plate_no}', 'CarController@show')->name('car');
+    Route::get('cars', 'CarController@index')->name('cars');
 
-Route::get('customers', 'CustomerController@index')->name('customers');
+    Route::get('cars/{car:plate_no}', 'CarController@show')->name('car');
 
-Route::get('customers/{customer:slug}', 'CustomerController@show')->name('customer');
+    Route::get('customers', 'CustomerController@index')->name('customers');
 
-Route::get('services', function () {
-    return view('private.services');
-})->name('services');
+    Route::get('customers/{customer:slug}', 'CustomerController@show')->name('customer');
 
-Route::get('services/service', function () {
-    return view('private.service');
-})->name('service');
+    Route::get('services', function () {
+        return view('private.services');
+    })->name('services');
 
-Route::get('packages', function () {
-    return view('private.packages');
-})->name('packages');
+    Route::get('services/service', function () {
+        return view('private.service');
+    })->name('service');
 
-Route::get('packages/package', function () {
-    return view('private.package');
-})->name('package');
+    Route::get('packages', function () {
+        return view('private.packages');
+    })->name('packages');
 
-Route::get('dashboard', function () {
-    return view('private.dashboard');
-})->name('dashboard');
+    Route::get('packages/package', function () {
+        return view('private.package');
+    })->name('package');
 
-Route::get('crews', function () {
-    return view('private.crews');
-})->name('crews');
+    Route::get('dashboard', function () {
+        return view('private.dashboard');
+    })->name('dashboard');
 
-Route::get('crews/crew', function () {
-    return view('private.crew');
-})->name('crew');
+    Route::get('crews', function () {
+        return view('private.crews');
+    })->name('crews');
 
-Route::get('promotions', function () {
-    return view('public.promotions');
-})->name('promotions');
+    Route::get('crews/crew', function () {
+        return view('private.crew');
+    })->name('crew');
+});
