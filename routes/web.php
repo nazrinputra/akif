480f099ddc9641bd85720be4259c41b212237bb5
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-15 11:59:47 +0800

    Route for cars

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::get('/', function () {
 
 Route::get('store/{store:slug}', 'StoreController@show')->name('store');
 
+Route::get('cars', 'CarController@index')->name('cars');
+
+Route::get('car/{car:plate_no}', 'CarController@show')->name('car');
+
 Route::get('counter', function () {
     return view('counter-static');
 });
