1b3e20c36e9c67c2c448fde0f0ef91a81f32cf29
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-15 16:49:02 +0800

    Seed customer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CarSeeder::class,
+            CustomerSeeder::class,
             StoreSeeder::class,
             QueueSeeder::class,
         ]);
