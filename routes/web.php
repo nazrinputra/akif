69b26ed26dd2cecf9d3fb9ee5a4153bb8d1f8f04
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-15 13:38:05 +0800

    Add routes for new views

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 Route::get('store/{store:slug}', 'StoreController@show')->name('store');
 
 Route::get('whatsapp', function () {
-    return view('whatsapp');
+    return view('dashboard.whatsapp');
 });
 
 Route::get('cars', 'CarController@index')->name('cars');
@@ -32,16 +32,52 @@ Route::get('cars', 'CarController@index')->name('cars');
 Route::get('car/{car:plate_no}', 'CarController@show')->name('car');
 
 Route::get('counter', function () {
-    return view('counter-static');
+    return view('dashboard.counter');
+});
+
+Route::get('customers', function () {
+    return view('dashboard.customers');
+});
+
+Route::get('customer', function () {
+    return view('dashboard.customer');
 });
 
 Route::get('services', function () {
-    return view('service-static');
+    return view('dashboard.services');
 });
 
 Route::get('service', function () {
-    return view('service-static');
+    return view('dashboard.service');
 });
+
+Route::get('packages', function () {
+    return view('dashboard.packages');
+});
+
+Route::get('package', function () {
+    return view('dashboard.package');
+});
+
+Route::get('dashboard', function () {
+    return view('dashboard.dashboard');
+});
+
+Route::get('crews', function () {
+    return view('dashboard.crews');
+});
+
+Route::get('crew', function () {
+    return view('dashboard.crew');
+});
+
+// Route::get('services', function () {
+//     return view('service-static');
+// });
+
+// Route::get('service', function () {
+//     return view('service-static');
+// });
 
 Auth::routes();
 
