9302d98fa227b13afc14b851a1e52385dc8b1e66
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-17 03:17:42 +0800

    Add vue to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,24 @@
-require('./bootstrap');
-require('./acw')
-var Turbolinks = require("turbolinks")
-Turbolinks.start()  
+import './bootstrap';
+import './acw';
+import Vue from 'vue';
+import Turbolinks from 'turbolinks';
+import TurbolinksAdapter from 'vue-turbolinks';
+
+Turbolinks.start();
+Vue.use(TurbolinksAdapter);
+Vue.component('test', {
+    template: 'components.Test.vue'
+});
+
+document.addEventListener('turbo:load', () => {
+    const element = document.getElementById("app");
+
+    if (element != null) {
+    
+        const app = new Vue({
+            el: element,
+            // template: '<App/>',
+            // components: { App }
+        });
+    }
+});
