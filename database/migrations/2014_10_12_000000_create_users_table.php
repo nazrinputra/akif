3ca92fec646bed96bbdfbba9b145bdfadf96927f
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-16 10:18:23 +0800

    Add uniques constraint on name that will be used for slug

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('slug')->unique();
             $table->integer('phone_no');
             $table->string('email')->unique();
