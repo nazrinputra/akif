7601d26500ca72f54fa2fc4bcf65d770b217fb8d
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-17 19:38:30 +0800

    Add route group and remove unnecessary props

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,13 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Welcome');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
 
 require __DIR__ . '/auth.php';
