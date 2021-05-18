c95f31ae1f550cfc6d9c1675e4dbd36b4856f315
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-18 17:51:18 +0800

    Add whatsapp to db

---
+++ app/Http/Controllers/WhatsappController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Whatsapp;
+use Illuminate\Http\Request;
+
+class WhatsappController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Whatsapp  $whatsapp
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Whatsapp $whatsapp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Whatsapp  $whatsapp
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Whatsapp $whatsapp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Whatsapp  $whatsapp
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Whatsapp $whatsapp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Whatsapp  $whatsapp
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Whatsapp $whatsapp)
+    {
+        //
+    }
+}
