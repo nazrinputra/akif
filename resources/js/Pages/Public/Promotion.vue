19e62ef3763c372d8d0cb42dc64e17c3d527770d
nisa-sabrina <nisa.sabrina94@yahoo.com>
2021-05-20 21:40:39 +0800

    Modified for promotion page

--- resources/js/Pages/Public/Promotion.vue
+++ resources/js/Pages/Public/Promotion.vue
@@ -37,51 +37,74 @@
         </li>
     </teleport>
     <section class="masthead">
-        <div class="container">
-            <div class="row mt-5">
-                <div class="col">
-                    <p><strong>Name:</strong> {{ promotion.name }}</p>
-                </div>
-                <div class="col">
-                    <p><strong>Price:</strong> RM{{ promotion.price }}</p>
-                </div>
-            </div>
-            <div class="row mt-5">
-                <div class="col">
-                    <p>
-                        <strong> Frequency:</strong> {{ promotion.frequency }}
-                    </p>
-                </div>
-                <div class="col">
-                    <p><strong>Duration:</strong>{{ promotion.duration }}</p>
-                </div>
-            </div>
-            <div class="row mt-5">
-                <div class="col">
-                    <p>
-                        <strong>Description:</strong>
+        <div class="container p-4">
+            <div
+                class="row featurette pt-5 shadow px-2 p-3 mb-5 bg-white rounded"
+            >
+                <div class="col-md-6">
+                    <h1 class="featurette-heading ">
+                        {{ promotion.name }}
+                    </h1>
+                    <h4>Price: RM {{ promotion.price }}</h4>
+                    <h5>
+                        <span class="text-muted">
+                            Frequency: {{ promotion.frequency }} <br />
+                            Duration: {{ promotion.duration }} <br
+                        /></span>
+                    </h5>
+                    <p class="lead">
                         {{ promotion.description }}
                     </p>
                 </div>
-            </div>
-            <div class="row mt-5">
-                <div class="col">
-                    <p><strong>Services:</strong></p>
-                    <ul>
-                        <li
-                            v-for="service in promotion.services"
-                            v-bind:key="service.id"
-                        >
-                            <p>
-                                <strong
-                                    >RM{{ service.price }}
-                                    {{ service.name }}</strong
+                <!-- <div class="col-md-5">
+                    <svg
+                        class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                        width="500"
+                        height="500"
+                        xmlns="http://www.w3.org/2000/svg"
+                        role="img"
+                        aria-label="Placeholder: 500x500"
+                        preserveAspectRatio="xMidYMid slice"
+                        focusable="false"
+                    >
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#eee" />
+                        <text x="50%" y="50%" fill="#aaa" dy=".3em">
+                            500x500
+                        </text>
+                    </svg>
+                </div> -->
+                <div id="accordion" class="col-md-6 pb-4">
+                    <div
+                        class="card"
+                        v-for="(service, index) in promotion.services"
+                        v-bind:key="service.id"
+                    >
+                        <div class="card-header" :id="'header' + index">
+                            <h5 class="mb-0">
+                                <button
+                                    class="btn btn-link"
+                                    data-toggle="collapse"
+                                    :data-target="'#body' + index"
+                                    aria-expanded="true"
+                                    :aria-controls="'body' + index"
                                 >
-                                -
+                                    {{ service.name }}
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div
+                            :id="'body' + index"
+                            class="collapse"
+                            :aria-labelledby="'header' + index"
+                            data-parent="#accordion"
+                        >
+                            <div class="card-body">
                                 {{ service.description }}
-                            </p>
-                        </li>
-                    </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
