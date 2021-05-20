bbfe43fc030c085c163e464fa64a085391c9fff5
nisa-sabrina <nisa.sabrina94@yahoo.com>
2021-05-20 14:52:18 +0800

    Add carousel to promotions

--- resources/js/Pages/Public/Promotions.vue
+++ resources/js/Pages/Public/Promotions.vue
@@ -37,51 +37,114 @@
         </li>
     </teleport>
     <section class="masthead">
-        <div class="container">
-            <div class="row mt-5">
-                <div
-                    class="col-xs-12 col-sm-6 col-md-4"
-                    v-for="promo in promotions"
-                    v-bind:key="promo.id"
-                >
-                    <div
-                        class="image-flip"
-                        ontouchstart="this.classList.toggle('hover');"
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li
+                    data-target="#myCarousel"
+                    data-slide-to="0"
+                    class="active"
+                ></li>
+                <li
+                    v-for="(promotion, index) in promotions"
+                    v-bind:key="promotion.id"
+                    data-target="#myCarousel"
+                    :data-slide-to="index"
+                ></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <svg
+                        class="bd-placeholder-img"
+                        width="100%"
+                        height="100%"
+                        xmlns="http://www.w3.org/2000/svg"
+                        role="img"
+                        aria-label=" :  "
+                        preserveAspectRatio="xMidYMid slice"
+                        focusable="false"
                     >
-                        <div class="mainflip">
-                            <div class="frontside">
-                                <div class="card">
-                                    <div class="card-body text-center">
-                                        <h4 class="card-title">
-                                            {{ promo.name }}
-                                        </h4>
-                                        <p class="card-text">
-                                            {{ promo.description }}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="backside">
-                                <div class="card">
-                                    <div class="card-body text-center mt-4">
-                                        <h4 class="card-title">
-                                            {{ promo.name }}
-                                        </h4>
-                                        <p class="card-text">
-                                            <ul>
-                                                <li v-for="service in promo.services"
-                                                v-bind:key="service.id">
-                                                {{ service.name }}</li>
-                                            </ul>
-                                        </p>
-                                        <inertia-link :href="route('promotion', promo.slug)" class="btn btn-outline-secondary">View More</inertia-link>
-                                    </div>
-                                </div>
-                            </div>
+                        <title></title>
+                        <rect width="100%" height="100%" fill="#777" />
+                        <text x="50%" y="50%" fill="#777" dy=".3em"></text>
+                    </svg>
+
+                    <div class="container">
+                        <div class="carousel-caption text-left">
+                            <h1>{{ firstPromotion.name }}</h1>
+                            <p>
+                                {{ firstPromotion.description }}
+                            </p>
+                            <p>
+                                <inertia-link
+                                    class="btn btn-lg btn-primary"
+                                    :href="route('promotion', firstPromotion)"
+                                    >View more</inertia-link
+                                >
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="carousel-item"
+                    v-for="promotion in promotions"
+                    v-bind:key="promotion.id"
+                >
+                    <svg
+                        class="bd-placeholder-img"
+                        width="100%"
+                        height="100%"
+                        xmlns="http://www.w3.org/2000/svg"
+                        role="img"
+                        aria-label=" :  "
+                        preserveAspectRatio="xMidYMid slice"
+                        focusable="false"
+                    >
+                        <title></title>
+                        <rect width="100%" height="100%" fill="#777" />
+                        <text x="50%" y="50%" fill="#777" dy=".3em"></text>
+                    </svg>
+
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1>{{ promotion.name }}</h1>
+                            <p>
+                                {{ promotion.description }}
+                            </p>
+                            <p>
+                                <inertia-link
+                                    class="btn btn-lg btn-primary"
+                                    :href="route('promotion', promotion)"
+                                    >View promotion</inertia-link
+                                >
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
+            <a
+                class="carousel-control-prev"
+                href="#myCarousel"
+                role="button"
+                data-slide="prev"
+            >
+                <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a
+                class="carousel-control-next"
+                href="#myCarousel"
+                role="button"
+                data-slide="next"
+            >
+                <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </section>
     <teleport to="#copyright">
@@ -97,12 +160,30 @@
     </teleport>
 </template>
 
+<style>
+.bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
+@media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+    }
+}
+</style>
+
 <script>
 export default {
     props: {
         auth: Object,
         errors: Object,
+        firstPromotion: Object,
         promotions: Object
-    },
+    }
 };
 </script>
