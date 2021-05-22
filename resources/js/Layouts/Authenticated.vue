c046118406bc5de4324436096b99569be6365be8
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-22 13:02:38 +0800

    Remove link if in dashboard page

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -11,11 +11,18 @@
                                 class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"
                             >
                                 <breeze-nav-link
+                                    v-if="!route().current('dashboard')"
                                     :href="route('dashboard')"
-                                    :active="route().current('dashboard')"
+                                    :active="true"
                                 >
                                     Dashboard
                                 </breeze-nav-link>
+                                <span
+                                    v-if="route().current('dashboard')"
+                                    class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
+                                >
+                                    Dashboard
+                                </span>
                                 <slot name="nav" />
                             </div>
                         </div>
@@ -123,12 +130,19 @@
                 >
                     <div class="pt-2 pb-3 space-y-1">
                         <breeze-responsive-nav-link
+                            v-if="!route().current('dashboard')"
                             :href="route('dashboard')"
-                            :active="route().current('dashboard')"
+                            :active="true"
                         >
                             Dashboard
                         </breeze-responsive-nav-link>
                         <slot name="responsive-nav" />
+                        <span
+                            v-if="route().current('dashboard')"
+                            class="block pl-3 pr-4 py-2 border-l-4 border-indigo-400 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out"
+                        >
+                            Dashboard
+                        </span>
                     </div>
 
                     <!-- Responsive Settings Options -->
