d8563d3214e1cae7dbd52d4c3c1b2213033a4095
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-24 11:25:52 +0800

    Separate middleware to it's own name

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
-            \App\Http\Middleware\RedirectSecure::class,
         ],
 
         'api' => [
@@ -64,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'trick.secure' => \App\Http\Middleware\RedirectSecure::class,
     ];
 }
