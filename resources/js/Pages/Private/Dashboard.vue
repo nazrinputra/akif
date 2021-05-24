d2507d705dd28e981f53a355a6270422b73f2ed7
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-24 19:36:50 +0800

    Add notice for dashbard

--- resources/js/Pages/Private/Dashboard.vue
+++ resources/js/Pages/Private/Dashboard.vue
@@ -8,114 +8,116 @@
                 Dashboard view for crew.
             </h6>
         </template>
-        <div class="max-w-7xl mx-auto px-3">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="row p-3">
-                        <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
-                            <h2>Counter</h2>
-                            <p>Register car and customer into queue.</p>
-                            <p>
-                                <inertia-link
-                                    class="btn btn-secondary"
-                                    :href="route('counter')"
-                                    >Counter</inertia-link
-                                >
-                            </p>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
-                            <h2>WhatsApps</h2>
-                            <p>List of WhatsApp messages for the store.</p>
-                            <p>
-                                <inertia-link
-                                    class="btn btn-secondary"
-                                    :href="route('whatsapps.index')"
-                                    >WhatsApps</inertia-link
-                                >
-                            </p>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
-                            <h2>Cars</h2>
-                            <p>
-                                View, add or edit cars registered in the system.
-                            </p>
-                            <p>
-                                <inertia-link
-                                    class="btn btn-secondary"
-                                    :href="route('cars.index')"
-                                    >Cars</inertia-link
-                                >
-                            </p>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
-                            <h2>Customers</h2>
-                            <p>
-                                Add new customer, view or update existing
-                                customers.
-                            </p>
-                            <p>
-                                <inertia-link
-                                    class="btn btn-secondary"
-                                    :href="route('customers.index')"
-                                    >Customers</inertia-link
-                                >
-                            </p>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
-                            <h2>Packages</h2>
-                            <p>
-                                Define packages or create new promotional
-                                package.
-                            </p>
-                            <p>
-                                <inertia-link
-                                    class="btn btn-secondary"
-                                    :href="route('packages.index')"
-                                    >Packages</inertia-link
-                                >
-                            </p>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
-                            <h2>Services</h2>
-                            <p>
-                                View services in the system and make
-                                modifications.
-                            </p>
-                            <p>
-                                <inertia-link
-                                    class="btn btn-secondary"
-                                    :href="route('services.index')"
-                                    >Services</inertia-link
-                                >
-                            </p>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
-                            <h2>Personalities</h2>
-                            <p>
-                                Manage customer personalities.
-                            </p>
-                            <p>
-                                <inertia-link
-                                    class="btn btn-secondary"
-                                    :href="route('personalities.index')"
-                                    >Personalities</inertia-link
-                                >
-                            </p>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
-                            <h2>Crews</h2>
-                            <p>
-                                Enable or disable staff access to the system.
-                            </p>
-                            <p>
-                                <inertia-link
-                                    class="btn btn-secondary"
-                                    :href="route('crews.index')"
-                                    >Crews</inertia-link
-                                >
-                            </p>
-                        </div>
-                    </div>
+        <div
+            class="px-6 pb-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+        >
+            <p class="text-center py-3">
+                <i class="fas fa-exclamation-triangle"></i
+                ><i
+                    >Dashboard should be use for queue, promo, owner,
+                    behaviour</i
+                ><i class="fas fa-exclamation-triangle"></i>
+            </p>
+            <div class="row p-3">
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
+                    <h2>Counter</h2>
+                    <p>Register car and customer into queue.</p>
+                    <p>
+                        <inertia-link
+                            class="btn btn-secondary"
+                            :href="route('counter')"
+                            >Counter</inertia-link
+                        >
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
+                    <h2>WhatsApps</h2>
+                    <p>List of WhatsApp messages for the store.</p>
+                    <p>
+                        <inertia-link
+                            class="btn btn-secondary"
+                            :href="route('whatsapps.index')"
+                            >WhatsApps</inertia-link
+                        >
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
+                    <h2>Cars</h2>
+                    <p>
+                        View, add or edit cars registered in the system.
+                    </p>
+                    <p>
+                        <inertia-link
+                            class="btn btn-secondary"
+                            :href="route('cars.index')"
+                            >Cars</inertia-link
+                        >
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
+                    <h2>Customers</h2>
+                    <p>
+                        Add new customer, view or update existing customers.
+                    </p>
+                    <p>
+                        <inertia-link
+                            class="btn btn-secondary"
+                            :href="route('customers.index')"
+                            >Customers</inertia-link
+                        >
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
+                    <h2>Packages</h2>
+                    <p>
+                        Define packages or create new promotional package.
+                    </p>
+                    <p>
+                        <inertia-link
+                            class="btn btn-secondary"
+                            :href="route('packages.index')"
+                            >Packages</inertia-link
+                        >
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
+                    <h2>Services</h2>
+                    <p>
+                        View services in the system and make modifications.
+                    </p>
+                    <p>
+                        <inertia-link
+                            class="btn btn-secondary"
+                            :href="route('services.index')"
+                            >Services</inertia-link
+                        >
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
+                    <h2>Personalities</h2>
+                    <p>
+                        Manage customer personalities.
+                    </p>
+                    <p>
+                        <inertia-link
+                            class="btn btn-secondary"
+                            :href="route('personalities.index')"
+                            >Personalities</inertia-link
+                        >
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-5">
+                    <h2>Crews</h2>
+                    <p>
+                        Enable or disable staff access to the system.
+                    </p>
+                    <p>
+                        <inertia-link
+                            class="btn btn-secondary"
+                            :href="route('crews.index')"
+                            >Crews</inertia-link
+                        >
+                    </p>
                 </div>
             </div>
         </div>
