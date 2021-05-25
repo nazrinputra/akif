5c94baf94f8205e900f9dec449e14fc773db3fdb
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-25 11:14:38 +0800

    Add profile page with allow for password change

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use App\Models\User;
+use Inertia\Inertia;
+use App\Models\Store;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        return Inertia::render('Private/Dashboard/Profile', [
+            'stores' => Store::all(),
+            'roles' => Role::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:50'],
+            'phone_no' => ['required', 'max:12'],
+            'email' => ['required', 'max:50'],
+            'store_id' => ['required'],
+            'role_id' => ['required'],
+            'password' => ['required', 'confirmed', 'min:8']
+        ]);
+
+        $slug = Str::slug($request->name);
+        $request->merge(['slug' => $slug]);
+
+        $password = Hash::make($request->password);
+        $request->merge(['password' => $password]);
+
+        Auth::user()->update($request->only('name', 'slug', 'phone_no', 'email', 'password', 'store_id', 'role_id'));
+
+        return Redirect::route('profiles.show')->with('success', 'Profile updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
--- resources/js/Pages/Private/Dashboard/Profile.vue
+++ resources/js/Pages/Private/Dashboard/Profile.vue
@@ -12,36 +12,177 @@
             </h6>
         </template>
         <template #nav>
-            <breeze-nav-link
-                :href="route('profile')"
-                :active="route().current('profile')"
+            <span
+                class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
             >
                 Profile
-            </breeze-nav-link>
-        </template>
-
-        <template #responsive-nav>
-            <breeze-responsive-nav-link
-                :href="route('profile')"
-                :active="route().current('profile')"
-            >
-                Profile
-            </breeze-responsive-nav-link>
+            </span>
         </template>
 
         <div
-            class="px-6 pb-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+            class="p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
         >
-            <div class="row p-3">
-                <div class="col pb-5">
-                    <h2>Profile</h2>
-                    <p>
-                        View, add or edit my profile.
-                    </p>
-                    <p>{{ user.name }}</p>
-                    <p>{{ user.store.name }}</p>
-                    <i class="fas fa-tools text-9xl"></i>
-                </div>
+            <div class="container">
+                <form @submit.prevent="submit">
+                    <div class="mt-3 p-3">
+                        <label for="name">Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            class="w-full rounded-md shadow-sm"
+                            :class="
+                                form.errors.name
+                                    ? 'border-red-500 focus:ring focus:ring-red-200 focus:ring-opacity-100'
+                                    : 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                            "
+                            v-model="form.name"
+                            @keydown="form.clearErrors('name')"
+                        />
+                        <span class="text-red-700 mt-2 text-sm">{{
+                            form.errors.name
+                        }}</span>
+                    </div>
+                    <div class="mt-3 p-3">
+                        <label for="phone_no">Phone No</label>
+                        <input
+                            type="number"
+                            id="phone_no"
+                            class="w-full rounded-md shadow-sm"
+                            :class="
+                                form.errors.phone_no
+                                    ? 'border-red-500 focus:ring focus:ring-red-200 focus:ring-opacity-100'
+                                    : 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                            "
+                            v-model="form.phone_no"
+                            @keydown="form.clearErrors('phone_no')"
+                        />
+                        <span class="text-red-700 mt-2 text-sm">{{
+                            form.errors.phone_no
+                        }}</span>
+                    </div>
+                    <div class="mt-3 p-3">
+                        <label for="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            class="w-full rounded-md shadow-sm"
+                            :class="
+                                form.errors.email
+                                    ? 'border-red-500 focus:ring focus:ring-red-200 focus:ring-opacity-100'
+                                    : 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                            "
+                            v-model="form.email"
+                            @keydown="form.clearErrors('email')"
+                        />
+                        <span class="text-red-700 mt-2 text-sm">{{
+                            form.errors.email
+                        }}</span>
+                    </div>
+                    <div class="mt-3 p-3">
+                        <label for="store_id">Store</label>
+                        <!-- TODO add role checking for disable-->
+                        <select
+                            v-model="form.store_id"
+                            :disabled="auth.user.role_id == 1"
+                            @change="form.clearErrors('store_id')"
+                            class="w-full rounded-md shadow-sm"
+                            :class="
+                                form.errors.store_id
+                                    ? 'border-red-500 focus:ring focus:ring-red-200 focus:ring-opacity-100'
+                                    : 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                            "
+                        >
+                            <option
+                                v-for="store in stores"
+                                :key="store.id"
+                                :value="store.id"
+                            >
+                                {{ store.name }}
+                            </option>
+                        </select>
+                        <span class="text-red-700 mt-2 text-sm">{{
+                            form.errors.store_id
+                        }}</span>
+                    </div>
+                    <div class="mt-3 p-3">
+                        <label for="role_id">Role</label>
+                        <!-- TODO add role checking for disable-->
+                        <select
+                            v-model="form.role_id"
+                            @change="form.clearErrors('role_id')"
+                            :disabled="auth.user.role_id == 1"
+                            class="w-full rounded-md shadow-sm"
+                            :class="
+                                form.errors.role_id
+                                    ? 'border-red-500 focus:ring focus:ring-red-200 focus:ring-opacity-100'
+                                    : 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                            "
+                        >
+                            <option
+                                v-for="role in roles"
+                                :key="role.id"
+                                :value="role.id"
+                            >
+                                {{ role.name }}
+                            </option>
+                        </select>
+                        <span class="text-red-700 mt-2 text-sm">{{
+                            form.errors.role_id
+                        }}</span>
+                    </div>
+                    <div class="mt-3 p-3">
+                        <label for="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            class="w-full rounded-md shadow-sm"
+                            :class="
+                                form.errors.password
+                                    ? 'border-red-500 focus:ring focus:ring-red-200 focus:ring-opacity-100'
+                                    : 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                            "
+                            v-model="form.password"
+                            @keydown="form.clearErrors('password')"
+                        />
+                        <span class="text-red-700 mt-2 text-sm">{{
+                            form.errors.password
+                        }}</span>
+                    </div>
+                    <div class="mt-3 p-3">
+                        <label for="password_confirmation"
+                            >Confirm Password</label
+                        >
+                        <input
+                            type="password"
+                            id="password_confirmation"
+                            class="w-full rounded-md shadow-sm"
+                            :class="
+                                form.errors.password
+                                    ? 'border-red-500 focus:ring focus:ring-red-200 focus:ring-opacity-100'
+                                    : 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                            "
+                            v-model="form.password_confirmation"
+                            @keydown="form.clearErrors('password')"
+                        />
+                        <span class="text-red-700 mt-2 text-sm">{{
+                            form.errors.password
+                        }}</span>
+                    </div>
+
+                    <div
+                        class="mt-3 p-3 bg-gray-50 border-t border-gray-100 row justify-between"
+                    >
+                        <breeze-button
+                            class="ml-auto"
+                            :class="{
+                                'opacity-25': form.processing
+                            }"
+                            :disabled="form.processing"
+                        >
+                            Update
+                        </breeze-button>
+                    </div>
+                </form>
             </div>
         </div>
     </breeze-authenticated-layout>
@@ -50,19 +191,56 @@
 <script>
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated";
 import BreezeNavLink from "@/Components/NavLink";
-import BreezeResponsiveNavLink from "@/Components/ResponsiveNavLink";
+import BreezeButton from "@/Components/Button";
+import { useForm } from "@inertiajs/inertia-vue3";
 
 export default {
     components: {
         BreezeAuthenticatedLayout,
         BreezeNavLink,
-        BreezeResponsiveNavLink
+        BreezeButton
     },
 
     props: {
         auth: Object,
         errors: Object,
-        user: Object
+        flash: Object,
+        stores: Object,
+        roles: Object
+    },
+
+    setup() {
+        const form = useForm({
+            name: null,
+            phone_no: null,
+            email: null,
+            store_id: null,
+            role_id: null,
+            password: null,
+            password_confirmation: null
+        });
+
+        return { form };
+    },
+
+    created() {
+        this.loadData();
+    },
+
+    methods: {
+        loadData() {
+            this.form.name = this.auth.user.name;
+            this.form.phone_no = this.auth.user.phone_no;
+            this.form.email = this.auth.user.email;
+            this.form.store_id = this.auth.user.store_id;
+            this.form.role_id = this.auth.user.role_id;
+        },
+        submit() {
+            this.form.put(route("profiles.update"), {
+                onSuccess: () =>
+                    this.form.reset("password", "password_confirmation")
+            });
+        }
     }
 };
 </script>
