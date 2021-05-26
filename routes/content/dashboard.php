3ee94937efcb135b82cc29b45a364ab2d0105db5
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-26 14:36:52 +0800

    Allow linking of owners

--- routes/content/dashboard.php
+++ routes/content/dashboard.php
@@ -2,9 +2,11 @@
 
 use App\Models\Car;
 use Inertia\Inertia;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redirect;
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('counter', function () {
@@ -16,13 +18,17 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
     Route::get('owner', function () {
         return Inertia::render('Private/Dashboard/Owner', [
-            'cars' => DB::table('cars')->orderBy('plate_no', 'asc')->get(),
-            'customers' => DB::table('customers')->orderBy('name', 'asc')->get()
+            'cars' => Car::select('*')->orderBy('model')->get(),
+            'customers' => Customer::select('*')->orderBy('name')->get(),
         ]);
     })->name('owner');
 
     Route::post('owner', function (Request $request) {
-        dd($request);
+        $customer = Customer::find($request->customer_id);
+        $car = Car::find($request->car_id);
+        $car->owners()->save($customer);
+
+        return Redirect::back()->with('success', 'Owner linked successfully. <a href="' . route('cars.show', $car) . '"style="color:#fff;text-decoration:underline;">View car</a> or <a href="' . route('customers.show', $customer) . '"style="color:#fff;text-decoration:underline;">view customer</a>');
     })->name('owner.store');
 
     Route::get('promotion', function () {
