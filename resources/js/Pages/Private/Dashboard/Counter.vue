ad824001c4cb287594348a2c43664d414a32e635
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-29 01:05:23 +0800

    Add simple stepper

--- resources/js/Pages/Private/Dashboard/Counter.vue
+++ resources/js/Pages/Private/Dashboard/Counter.vue
@@ -20,14 +20,48 @@
         </template>
 
         <div
-            class="px-6 pb-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+            class="px-6 pb-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg md-stepper-horizontal"
         >
-            <div class="p-3">
-                <img
-                    class="img-fluid w-40 h-40 py-20"
-                    src="\fonts\vendor\@fortawesome\fontawesome-free\tools.svg"
-                />
-                <h4 class="text-center">coming soon</h4>
+            <div
+                class="md-step cursor-pointer"
+                :class="stepper.stepOne ? 'active' : ''"
+                @click="stepOne"
+            >
+                <div class="md-step-circle"><span>1</span></div>
+                <div class="md-step-title">Car</div>
+                <div class="md-step-bar-left"></div>
+                <div class="md-step-bar-right"></div>
+            </div>
+            <div
+                class="md-step cursor-pointer"
+                :class="stepper.stepTwo ? 'active' : ''"
+                @click="stepTwo"
+            >
+                <div class="md-step-circle"><span>2</span></div>
+                <div class="md-step-title">Customer</div>
+                <div class="md-step-bar-left"></div>
+                <div class="md-step-bar-right"></div>
+            </div>
+            <div
+                class="md-step cursor-pointer"
+                :class="stepper.stepThree ? 'active' : ''"
+                @click="stepThree"
+            >
+                <div class="md-step-circle"><span>3</span></div>
+                <div class="md-step-title">Package</div>
+                <div class="md-step-optional">Optional</div>
+                <div class="md-step-bar-left"></div>
+                <div class="md-step-bar-right"></div>
+            </div>
+            <div
+                class="md-step cursor-pointer"
+                :class="stepper.stepFour ? 'active' : ''"
+                @click="stepFour"
+            >
+                <div class="md-step-circle"><span>4</span></div>
+                <div class="md-step-title">Review</div>
+                <div class="md-step-bar-left"></div>
+                <div class="md-step-bar-right"></div>
             </div>
         </div>
     </breeze-authenticated-layout>
@@ -37,7 +71,6 @@
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated";
 import BreezeNavLink from "@/Components/NavLink";
 import BreezeButton from "@/Components/Button";
-import { useForm } from "@inertiajs/inertia-vue3";
 
 export default {
     components: {
@@ -49,18 +82,137 @@ export default {
     props: {
         auth: Object,
         errors: Object,
-        flash: Object,
-        cars: Object,
-        packages: Object
+        flash: Object
     },
 
-    setup() {
-        const form = useForm({
-            car_id: null,
-            package_id: null
-        });
+    data() {
+        return {
+            stepper: {
+                stepOne: true,
+                stepTwo: false,
+                stepThree: false,
+                stepFour: false
+            }
+        };
+    },
 
-        return { form };
+    methods: {
+        stepOne() {
+            this.stepper.stepTwo = false;
+            this.stepper.stepThree = false;
+            this.stepper.stepFour = false;
+        },
+        stepTwo() {
+            this.stepper.stepOne = true;
+            this.stepper.stepTwo = true;
+            this.stepper.stepThree = false;
+            this.stepper.stepFour = false;
+        },
+        stepThree() {
+            this.stepper.stepOne = true;
+            this.stepper.stepTwo = true;
+            this.stepper.stepThree = true;
+            this.stepper.stepFour = false;
+        },
+        stepFour() {
+            this.stepper.stepOne = true;
+            this.stepper.stepTwo = true;
+            this.stepper.stepThree = true;
+            this.stepper.stepFour = true;
+        }
     }
 };
 </script>
+
+<style>
+.md-stepper-horizontal {
+    display: table;
+    width: 100%;
+    margin: 0 auto;
+    background-color: #ffffff;
+    box-shadow: 0 3px 8px -6px rgba(0, 0, 0, 0.5);
+}
+.md-stepper-horizontal .md-step {
+    display: table-cell;
+    position: relative;
+    padding: 24px;
+}
+.md-stepper-horizontal .md-step:hover,
+.md-stepper-horizontal .md-step:active {
+    background-color: rgba(0, 0, 0, 0.04);
+}
+.md-stepper-horizontal .md-step:active {
+    border-radius: 15% / 75%;
+}
+.md-stepper-horizontal .md-step:first-child:active {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+}
+.md-stepper-horizontal .md-step:last-child:active {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+}
+.md-stepper-horizontal .md-step:hover .md-step-circle {
+    background-color: #757575;
+}
+.md-stepper-horizontal .md-step:first-child .md-step-bar-left,
+.md-stepper-horizontal .md-step:last-child .md-step-bar-right {
+    display: none;
+}
+.md-stepper-horizontal .md-step .md-step-circle {
+    width: 30px;
+    height: 30px;
+    margin: 0 auto;
+    background-color: #999999;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 30px;
+    font-size: 16px;
+    font-weight: 600;
+    color: #ffffff;
+}
+.md-stepper-horizontal .md-step.active .md-step-circle {
+    background-color: #6dbcdb;
+}
+.md-stepper-horizontal .md-step .md-step-title {
+    margin-top: 16px;
+    font-size: 16px;
+    font-weight: 600;
+}
+.md-stepper-horizontal .md-step .md-step-title,
+.md-stepper-horizontal .md-step .md-step-optional {
+    text-align: center;
+    color: rgba(0, 0, 0, 0.26);
+}
+.md-stepper-horizontal .md-step.active .md-step-title {
+    font-weight: 600;
+    color: rgba(0, 0, 0, 0.87);
+}
+.md-stepper-horizontal .md-step.active.done .md-step-title,
+.md-stepper-horizontal .md-step.active.editable .md-step-title {
+    font-weight: 600;
+}
+.md-stepper-horizontal .md-step .md-step-optional {
+    font-size: 12px;
+}
+.md-stepper-horizontal .md-step.active .md-step-optional {
+    color: rgba(0, 0, 0, 0.54);
+}
+.md-stepper-horizontal .md-step .md-step-bar-left,
+.md-stepper-horizontal .md-step .md-step-bar-right {
+    position: absolute;
+    top: 36px;
+    height: 1px;
+    border-top: 1px solid #dddddd;
+}
+.md-stepper-horizontal .md-step .md-step-bar-right {
+    right: 0;
+    left: 50%;
+    margin-left: 20px;
+}
+.md-stepper-horizontal .md-step .md-step-bar-left {
+    left: 0;
+    right: 50%;
+    margin-right: 20px;
+}
+</style>
