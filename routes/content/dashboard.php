5f7a14bb3287211b64ab10d0dc14607a2b9d4277
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-29 01:05:11 +0800

    Return simple render

--- routes/content/dashboard.php
+++ routes/content/dashboard.php
@@ -1,15 +1,10 @@
 <?php
 
-use App\Models\Car;
 use Inertia\Inertia;
-use App\Models\Package;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('counter', function () {
-        return Inertia::render('Private/Dashboard/Counter', [
-            'cars' => Car::select('*')->orderBy('model')->get(),
-            'packages' => Package::select('*')->orderBy('name')->get(),
-        ]);
+        return Inertia::render('Private/Dashboard/Counter');
     })->name('counter');
 });
