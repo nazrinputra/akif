57f1412016713c565f9b369497a9b346c21cf095
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-28 21:04:32 +0800

    Allow link package when creating service

--- resources/js/Pages/Private/Service/Create.vue
+++ resources/js/Pages/Private/Service/Create.vue
@@ -29,7 +29,7 @@
             class="p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
         >
             <div class="container">
-                <form @submit.prevent="form.post(route('services.store'))">
+                <form @submit.prevent="submit">
                     <div class="mt-3 p-3">
                         <label for="name">Name</label>
                         <input
@@ -87,6 +87,86 @@
                             form.errors.description
                         }}</span>
                     </div>
+                    <div class="mt-3 p-3">
+                        <label for="search">Package</label>
+                        <div v-if="!pkg" class="input-group">
+                            <input
+                                type="text"
+                                id="search"
+                                placeholder="Search package..."
+                                class="col rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                v-model="formPackage.query"
+                            />
+                        </div>
+
+                        <transition name="fade">
+                            <div
+                                v-if="packages.length > 0"
+                                class="p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+                            >
+                                <table class="w-full whitespace-nowrap">
+                                    <tr class="text-left font-bold">
+                                        <th class="px-3 py-3">Package Name</th>
+                                    </tr>
+                                    <tr
+                                        v-for="pkg in packages"
+                                        :key="pkg.id"
+                                        class="hover:bg-gray-100 focus-within:bg-gray-100"
+                                    >
+                                        <td
+                                            class="border-t pl-3 py-3 flex items-center focus:text-indigo-500"
+                                        >
+                                            {{ pkg.name }}
+                                        </td>
+                                        <td
+                                            class="border-t w-px md:table-cell hidden pr-3"
+                                        >
+                                            <breeze-button
+                                                @click="selectPackage(pkg)"
+                                                type="button"
+                                            >
+                                                <i class="fas fa-check"></i>
+                                            </breeze-button>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </transition>
+
+                        <transition name="fade">
+                            <div
+                                v-if="pkg"
+                                class="p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+                            >
+                                <table class="w-full whitespace-nowrap">
+                                    <tr class="text-left font-bold">
+                                        <th class="px-3 py-3">
+                                            Selected Package Name
+                                        </th>
+                                    </tr>
+                                    <tr
+                                        class="hover:bg-gray-100 focus-within:bg-gray-100"
+                                    >
+                                        <td
+                                            class="border-t pl-3 py-3 flex items-center focus:text-indigo-500"
+                                        >
+                                            {{ pkg.name }}
+                                        </td>
+                                        <td
+                                            class="border-t w-px md:table-cell hidden pr-3"
+                                        >
+                                            <breeze-button
+                                                @click="clearPackage()"
+                                                type="button"
+                                            >
+                                                <i class="fas fa-times"></i>
+                                            </breeze-button>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </transition>
+                    </div>
                     <div
                         class="mt-3 p-3 bg-gray-50 border-t border-gray-100 row justify-between"
                     >
@@ -115,6 +195,7 @@
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated";
 import BreezeNavLink from "@/Components/NavLink";
 import BreezeButton from "@/Components/Button";
+import throttle from "lodash/throttle";
 import { useForm } from "@inertiajs/inertia-vue3";
 
 export default {
@@ -134,10 +215,59 @@ export default {
         const form = useForm({
             name: null,
             price: null,
-            description: null
+            description: null,
+            package_id: null
         });
 
         return { form };
+    },
+
+    data() {
+        return {
+            formPackage: {
+                query: null
+            },
+            packages: [],
+            pkg: null
+        };
+    },
+
+    watch: {
+        formPackage: {
+            deep: true,
+            handler: throttle(function() {
+                if (this.formPackage.query && this.formPackage.query != "") {
+                    axios
+                        .get(route("packages.search"), {
+                            params: {
+                                query: this.formPackage.query
+                            }
+                        })
+                        .then(response => {
+                            this.packages = response.data;
+                        });
+                } else {
+                    this.packages = [];
+                }
+            }, 150)
+        }
+    },
+
+    methods: {
+        selectPackage(pkg) {
+            this.pkg = pkg;
+            this.formPackage.query = "";
+            this.packages = [];
+        },
+        clearPackage() {
+            this.pkg = null;
+        },
+        submit() {
+            if (this.pkg) {
+                this.form.package_id = this.pkg.id;
+            }
+            this.form.post(route("services.store"));
+        }
     }
 };
 </script>
