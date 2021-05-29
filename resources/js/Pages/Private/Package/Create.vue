2212b0f05b3d50ae81ce4488979af573a7daa475
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-29 22:33:24 +0800

    Allow to add services when creating packages

--- resources/js/Pages/Private/Package/Create.vue
+++ resources/js/Pages/Private/Package/Create.vue
@@ -29,7 +29,7 @@
             class="p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
         >
             <div class="container">
-                <form @submit.prevent="form.post(route('packages.store'))">
+                <form @submit.prevent="submit">
                     <div class="mt-3 p-3">
                         <label for="name">Name</label>
                         <input
@@ -151,6 +151,103 @@
                             form.errors.promotion
                         }}</span>
                     </div>
+
+                    <div class="mt-3 p-3">
+                        <label for="search">Services</label>
+                        <div class="input-group">
+                            <input
+                                type="text"
+                                id="search"
+                                placeholder="Search service..."
+                                class="col rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                v-model="formService.query"
+                            />
+                        </div>
+
+                        <transition name="fade">
+                            <div
+                                v-if="services.length > 0"
+                                class="mb-3 p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+                            >
+                                <table class="w-full whitespace-nowrap">
+                                    <tr class="text-left font-bold">
+                                        <th class="px-3 py-3">Service Name</th>
+                                    </tr>
+                                    <tr
+                                        v-for="service in services"
+                                        :key="service.id"
+                                        class="hover:bg-gray-100 focus-within:bg-gray-100"
+                                    >
+                                        <td
+                                            class="border-t pl-3 py-3 flex items-center focus:text-indigo-500"
+                                        >
+                                            {{ service.name }}
+                                        </td>
+                                        <td
+                                            class="border-t w-px md:table-cell hidden pr-3"
+                                        >
+                                            <breeze-button
+                                                v-if="
+                                                    !selectedServices.some(
+                                                        data =>
+                                                            data.id ===
+                                                            service.id
+                                                    )
+                                                "
+                                                type="button"
+                                                @click="selectService(service)"
+                                                tabindex="-1"
+                                            >
+                                                <i class="fas fa-check"></i>
+                                            </breeze-button>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </transition>
+
+                        <div
+                            v-if="selectedServices.length > 0"
+                            class="p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+                        >
+                            <table class="w-full whitespace-nowrap">
+                                <tr class="text-left font-bold">
+                                    <th class="px-3 py-3">
+                                        Selected Service Name
+                                    </th>
+                                </tr>
+                                <tr
+                                    v-for="(service, index) in selectedServices"
+                                    :key="service.id"
+                                    class="hover:bg-gray-100 focus-within:bg-gray-100"
+                                >
+                                    <td class="border-t">
+                                        <inertia-link
+                                            style="color: inherit; text-decoration: inherit;"
+                                            class="px-3 py-3 flex items-center focus:text-indigo-500"
+                                            :href="
+                                                route('services.show', service)
+                                            "
+                                        >
+                                            {{ service.name }}
+                                        </inertia-link>
+                                    </td>
+                                    <td
+                                        class="border-t w-px md:table-cell hidden pr-3"
+                                    >
+                                        <breeze-button
+                                            type="button"
+                                            @click="removeService(index)"
+                                            tabindex="-1"
+                                        >
+                                            <i class="fas fa-times"></i>
+                                        </breeze-button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+
                     <div
                         class="mt-3 p-3 bg-gray-50 border-t border-gray-100 row justify-between"
                     >
@@ -179,6 +276,7 @@
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated";
 import BreezeNavLink from "@/Components/NavLink";
 import BreezeButton from "@/Components/Button";
+import throttle from "lodash/throttle";
 import { useForm } from "@inertiajs/inertia-vue3";
 
 export default {
@@ -201,10 +299,61 @@ export default {
             frequency: null,
             duration: null,
             description: null,
-            promotion: ""
+            promotion: "",
+            services: []
         });
 
         return { form };
+    },
+
+    data() {
+        return {
+            formService: {
+                query: null
+            },
+            services: [],
+            selectedServices: []
+        };
+    },
+
+    watch: {
+        formService: {
+            deep: true,
+            handler: throttle(function() {
+                if (this.formService.query && this.formService.query != "") {
+                    axios
+                        .get(route("services.search"), {
+                            params: {
+                                query: this.formService.query
+                            }
+                        })
+                        .then(response => {
+                            this.services = response.data;
+                        });
+                } else {
+                    this.services = [];
+                }
+            }, 150)
+        }
+    },
+
+    methods: {
+        selectService(service) {
+            this.formService.query = "";
+            this.services = [];
+            this.selectedServices.push(service);
+        },
+        removeService(index) {
+            this.selectedServices.splice(index, 1);
+        },
+        submit() {
+            if (this.selectedServices.length > 0) {
+                this.form.services = this.selectedServices.map(
+                    service => service.id
+                );
+            }
+            this.form.post(route("packages.store"));
+        }
     }
 };
 </script>
