86089e03e231ee2d763db9376f20b1bb4dbde66f
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-30 15:18:44 +0800

    Allow remark to be null

--- database/migrations/2021_05_13_063919_create_queues_table.php
+++ database/migrations/2021_05_13_063919_create_queues_table.php
@@ -19,7 +19,7 @@ class CreateQueuesTable extends Migration
             $table->foreignId('car_id')->constrained();
             $table->foreignId('customer_id')->constrained();
             $table->string('status');
-            $table->string('remarks');
+            $table->string('remarks')->nullable();
             $table->timestamps();
         });
     }
