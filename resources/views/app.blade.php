994667960a3648acd23bf75f918c22babfee91ad
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-05-30 09:25:45 +0800

    Remove unused meta tag?

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Akif</title>
