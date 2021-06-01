14aba68a0573d5760a1d490eea7d9f709c93f6d2
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-06-01 20:58:39 +0800

    Add permission checking to route

--- routes/web/role.php
+++ routes/web/role.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
 
 Route::group(
-    ['middleware' => ['auth', 'verified']],
+    ['middleware' => ['auth', 'verified', 'permission:view_roles|create_roles|edit_roles|reopen_roles']],
     function () {
         Route::resource('roles', RoleController::class);
 
