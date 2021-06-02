84da5a7a69890db66a94b5ade06abd346b4a9982
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-06-03 02:17:29 +0800

    Fix inertia request

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -35,7 +35,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => $request->user()->load('store'),
+                'user' => $request->user(),
                 'role' => $request->user() ? $request->user()->roles->first() : null,
                 'can' => $request->user() ? $request->user()->getPermissionArray() : [],
             ],
