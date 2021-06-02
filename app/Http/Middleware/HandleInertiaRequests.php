70cbcf262d207c95184088c7f01a0ef56594f882
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-06-03 01:57:09 +0800

    Load user store with all request

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -35,7 +35,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => $request->user(),
+                'user' => $request->user()->load('store'),
                 'role' => $request->user() ? $request->user()->roles->first() : null,
                 'can' => $request->user() ? $request->user()->getPermissionArray() : [],
             ],
