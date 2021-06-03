cb1d9e5e2e6c7d75ad117796b066081f35d5288c
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-06-03 22:13:37 +0800

    Seed default whatsapp

--- database/seeders/WhatsappSeeder.php
+++ database/seeders/WhatsappSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Whatsapp;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class WhatsappSeeder extends Seeder
@@ -14,6 +15,24 @@ class WhatsappSeeder extends Seeder
      */
     public function run()
     {
-        Whatsapp::factory(20)->create();
+        $completedTitle = 'Completed Message';
+
+        $completedSlug = Str::slug($completedTitle);
+
+        $completedMessage = urldecode('Pelanggan yang dihormati, terima kasih kerana memilih Akif Car Grooming Center. Kami berharap anda berpuas hati dengan servis yang diberikan. Sekiranya terdapat sebarang kekurangan, sila maklumkan kepada kami untuk penambahbaikkan.
+
+Anda boleh mengikuti perkembangan dan promosi terkini dari kami melalui pautan facebook ini, www.facebook.com/akifcarwash
+
+_______
+
+Dear valued customers, thank you for choosing Akif Car Grooming Center. We sincerely hope you are happy with the services we have provided. Should there be any dissatisfaction with any of the services, please do not hesitate to contact us about it.
+
+You can follow us here on facebook, www.facebook.com/akifcarwash , for the latest information and promotions.');
+
+        Whatsapp::create([
+            'title' => $completedTitle,
+            'slug' => $completedSlug,
+            'message' => $completedMessage
+        ]);
     }
 }
