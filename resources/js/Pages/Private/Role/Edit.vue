015164d7c648a553e51104ad1c94af778a5ae341
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-06-04 21:14:32 +0800

    List all permissions option when editing role

--- resources/js/Pages/Private/Role/Edit.vue
+++ resources/js/Pages/Private/Role/Edit.vue
@@ -86,6 +86,12 @@
             class="mb-3 p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
         >
             Oops, we could not find any matching permissions.
+            <span
+                @click="viewAllPermissions"
+                class="text-blue-500 text-decoration-none cursor-pointer"
+            >
+                View all?
+            </span>
         </div>
 
         <transition name="fade">
@@ -353,6 +359,11 @@ export default {
                     }
                 }
             );
+        },
+        viewAllPermissions() {
+            axios.get(route("permissions.all")).then(response => {
+                this.permissions = response.data;
+            });
         }
     },
 
