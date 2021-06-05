9d92848483f13a1c78d49729c1526ec77e8e4665
Nazrin.Putra <nazrin.putra@tm.com.my>
2021-06-06 01:44:41 +0800

    Add function to link health conditions

--- resources/js/Components/Crew/Health.vue
+++ resources/js/Components/Crew/Health.vue
@@ -21,11 +21,155 @@
                 form.errors.oku_card
             }}</span>
         </div>
+
+        <div class="mt-3 p-3">
+            <label for="search">Health Conditions</label>
+            <div class="input-group">
+                <input
+                    type="text"
+                    id="search"
+                    placeholder="Search health condition..."
+                    class="col rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                    v-model="formHealth.query"
+                />
+            </div>
+
+            <div
+                v-if="formHealth.query && healths.length == 0"
+                class="mb-3 p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+            >
+                Oops, we could not find any matching health condition.
+            </div>
+
+            <transition name="fade">
+                <div
+                    v-if="healths.length > 0"
+                    class="mb-3 p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+                >
+                    <table class="w-full whitespace-nowrap">
+                        <tr class="text-left font-bold">
+                            <th class="px-3 py-3">Health Condition</th>
+                        </tr>
+                        <tr
+                            v-for="health in healths"
+                            :key="health.id"
+                            class="hover:bg-gray-100 focus-within:bg-gray-100"
+                        >
+                            <td
+                                class="border-t pl-3 py-3 flex items-center focus:text-indigo-500"
+                            >
+                                {{ health.name }}
+                            </td>
+                            <td class="border-t w-px md:table-cell hidden pr-3">
+                                <breeze-button
+                                    v-if="
+                                        !selectedHealths.some(
+                                            data => data.id === health.id
+                                        )
+                                    "
+                                    type="button"
+                                    @click="selectHealth(health)"
+                                    tabindex="-1"
+                                >
+                                    <i class="fas fa-check"></i>
+                                </breeze-button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </transition>
+
+            <div
+                v-if="selectedHealths.length > 0"
+                class="p-6 bg-white border-b border-gray-200 max-w-7xl shadow sm:rounded-lg"
+            >
+                <table class="w-full whitespace-nowrap">
+                    <tr class="text-left font-bold">
+                        <th class="px-3 py-3">
+                            Selected Health Condition
+                        </th>
+                    </tr>
+                    <tr
+                        v-for="(health, index) in selectedHealths"
+                        :key="health.id"
+                        class="hover:bg-gray-100 focus-within:bg-gray-100"
+                    >
+                        <td class="border-t">
+                            <inertia-link
+                                style="color: inherit; text-decoration: inherit;"
+                                class="px-3 py-3 flex items-center focus:text-indigo-500"
+                                :href="route('healths.show', health)"
+                            >
+                                {{ health.name }}
+                            </inertia-link>
+                        </td>
+                        <td class="border-t w-px md:table-cell hidden pr-3">
+                            <breeze-button
+                                type="button"
+                                @click="removeHealth(index)"
+                                tabindex="-1"
+                            >
+                                <i class="fas fa-times"></i>
+                            </breeze-button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import BreezeButton from "@/Components/Button";
+import throttle from "lodash/throttle";
+
 export default {
-    props: ["form"]
+    props: ["form"],
+
+    components: {
+        BreezeButton
+    },
+
+    data() {
+        return {
+            formHealth: {
+                query: null
+            },
+            healths: [],
+            selectedHealths: []
+        };
+    },
+
+    watch: {
+        formHealth: {
+            deep: true,
+            handler: throttle(function() {
+                if (this.formHealth.query && this.formHealth.query != "") {
+                    axios
+                        .get(route("healths.search"), {
+                            params: {
+                                query: this.formHealth.query
+                            }
+                        })
+                        .then(response => {
+                            this.healths = response.data;
+                        });
+                } else {
+                    this.healths = [];
+                }
+            }, 150)
+        }
+    },
+
+    methods: {
+        selectHealth(health) {
+            this.formHealth.query = "";
+            this.healths = [];
+            this.selectedHealths.push(health);
+        },
+        removeHealth(index) {
+            this.selectedHealths.splice(index, 1);
+        }
+    }
 };
 </script>
