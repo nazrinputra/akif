6e1c323a021ae545178b74d59a5fe8febd8dac93
nazrinputra <putra_naz94@yahoo.com>
2021-06-13 16:53:16 +0800

    Move functions to controller

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,10 @@ use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QueueController;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PromotionController;
 
 /*
@@ -36,7 +38,7 @@ require __DIR__ . '/web/role.php';
 require __DIR__ . '/web/health.php';
 require __DIR__ . '/web/commission.php';
 
-URL::forceScheme('https');
+URL::forceScheme('https'); // FIXME might not need this?
 
 Route::get('/', function () {
     return Inertia::render('Public/Welcome');
@@ -49,57 +51,15 @@ Route::resource('promotions', PromotionController::class)->parameters([
 Route::get('store/{store:slug}', [StoreController::class, 'show'])->name('stores.show');
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('dashboard', function () {
-        $customers = Customer::all()->count();
-        $monthly = Queue::whereDate('created_at', '>', now()->subDays(30))->distinct()->pluck('car_id')->count();
-        $fresh = Customer::whereDate('created_at', '>', now()->subDays(30))->count();
-        $new = Customer::whereDate('created_at', '>', now()->subDays(60))->pluck('id')->toArray();
-        $stale = Queue::whereDate('created_at', '<', now()->subDays(60))->whereNotIn('customer_id', $new)->count();
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-        return Inertia::render('Private/Dashboard/Index', [
-            'customers' => $customers,
-            'monthly' => $monthly,
-            'fresh' => $fresh,
-            'stale' => $stale,
-            'stores' => Store::all(),
-            'roles' => Role::all(),
-            'healths' => Auth::user()->healths,
-        ]);
-    })->name('dashboard');
+    Route::get('monthly', [DashboardController::class, 'monthly'])->name('monthly')->middleware(['can:view queues']);
 
-    Route::get('monthly', function () {
-        $queues = Queue::where('created_at', '>', now()->subDays(30))->with('car')->paginate(10)->withQueryString();
+    Route::get('fresh', [DashboardController::class, 'fresh'])->name('fresh')->middleware(['can:view customers']);
 
-        return Inertia::render('Private/Dashboard/Monthly', [
-            'queues' => $queues
-        ]);
-    })->name('monthly')->middleware(['can:view queues']);
+    Route::get('stale', [DashboardController::class, 'stale'])->name('stale')->middleware(['can:view customers']);
 
-    Route::get('fresh', function () {
-        $customers = Customer::whereDate('created_at', '>', now()->subDays(30))->paginate(10)->withQueryString();
-
-        return Inertia::render('Private/Dashboard/Fresh', [
-            'customers' => $customers
-        ]);
-    })->name('fresh')->middleware(['can:view customers']);
-
-    Route::get('stale', function () {
-        $new = Customer::whereDate('created_at', '>', now()->subDays(60))->pluck('id')->toArray();
-        $stale = Queue::whereDate('created_at', '<', now()->subDays(60))->whereNotIn('customer_id', $new)->pluck('id')->toArray();
-        $customers = Customer::whereIn('id', $stale)->paginate(10)->withQueryString();
-
-        return Inertia::render('Private/Dashboard/Stale', [
-            'customers' => $customers
-        ]);
-    })->name('stale')->middleware(['can:view customers']);
-
-    Route::get('counter', function () {
-        return Inertia::render('Private/Dashboard/Counter');
-    })->name('counter')->middleware(['can:create queues']);
-
-    Route::get('reports', function () {
-        return Inertia::render('Private/Dashboard/Report');
-    })->name('reports')->middleware(['can:view report']);
+    Route::get('counter', [QueueController::class, 'create'])->name('counter')->middleware(['can:create queues']);
 
     Route::get('profiles', [ProfileController::class, 'show'])->name('profiles.show');
 
