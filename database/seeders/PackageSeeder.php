b0fce7da0309ffb273dfe6b066a34c777bb7dac5
nazrinputra <putra_naz94@yahoo.com>
2021-06-13 17:58:26 +0800

    Seed default package name

--- database/seeders/PackageSeeder.php
+++ database/seeders/PackageSeeder.php
@@ -14,6 +14,54 @@ class PackageSeeder extends Seeder
      */
     public function run()
     {
-        Package::factory(15)->create();
+        $faker = \Faker\Factory::create('ms_MY');
+
+        Package::create([
+            'slug' => 'signature-wash',
+            'name' => 'Signature Wash',
+            'custom_price' => false,
+            'price' => 3000,
+            'commission' => 300,
+            'frequency' => $faker->words(3, true),
+            'duration' => $faker->words(3, true),
+            'description' => $faker->sentences(5, true),
+            'promotion' => true,
+        ]);
+
+        Package::create([
+            'slug' => 'signature-wax',
+            'name' => 'Signature Wax',
+            'custom_price' => false,
+            'price' => 10000,
+            'commission' => 1000,
+            'frequency' => $faker->words(3, true),
+            'duration' => $faker->words(3, true),
+            'description' => $faker->sentences(5, true),
+            'promotion' => true,
+        ]);
+
+        Package::create([
+            'slug' => 'signature-scrub',
+            'name' => 'Signature Scrub',
+            'custom_price' => false,
+            'price' => 35000,
+            'commission' => 3500,
+            'frequency' => $faker->words(3, true),
+            'duration' => $faker->words(3, true),
+            'description' => $faker->sentences(5, true),
+            'promotion' => true,
+        ]);
+
+        Package::create([
+            'slug' => 'signature-polish',
+            'name' => 'Signature Polish',
+            'custom_price' => true,
+            'price' => 15000,
+            'commission' => null,
+            'frequency' => $faker->words(3, true),
+            'duration' => $faker->words(3, true),
+            'description' => $faker->sentences(5, true),
+            'promotion' => true,
+        ]);
     }
 }
